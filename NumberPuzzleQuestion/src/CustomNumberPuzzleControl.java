1ce7cc0fe12b96d4e7a3219efd794dbfd84a6090
DESKTOP-KGAIMJL\Hp-user <nehra7991@gmail.com>
2021-07-10 15:02:39 +0530

    Rohit Nehra number puzzle solution

--- NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
+++ NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
@@ -36,7 +36,15 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		Button buttonClicked = game.getButtonClicked();
 		Button[] buttons = game.getButtons();
 		
-		//Your logic here		
+		//Your logic here	
+		int i = 0;
+		for(Button button : buttons) {
+			if(buttonClicked == button) {
+				break;
+			} else i++;
+		}
+		swapButton(buttons[emptyCellId], buttonClicked);
+		emptyCellId = i;
 		
 		return emptyCellId;
 
@@ -45,8 +53,27 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		int arr[] = new int[15];
 		
 		//Your logic here
-		int a = getRandomNumber();
+		//int a = getRandomNumber();
+		for(int i = 0; i < 15; i++) {
+			arr[i] = -1;
+		}
 		
+		//int totalNos = 0;
+		int n = 0;
+		while(n != 15) {
+			int flag = 0;
+			int a = getRandomNumber();
+			a = a%15;
+			if( a == 0) a = 15;
+			for(int i = 0; i  < n; i++) {
+				if(arr[i] == a) flag = 1;
+			}
+			if(flag == 0) {
+				arr[n] = a;
+				System.out.print(a + " ");
+				n++;
+			}
+		}
 		
 		return arr;
 	}
@@ -55,8 +82,16 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		boolean winner = true;
 		
 		// Your Logic here
-		getIntegerArrayOfButtonIds(buttons);
-
+		int nos[] = getIntegerArrayOfButtonIds(buttons);
+		int flag = 0;
+		for(int i = 0; i < nos.length; i++) {
+			if(nos[i] != i+1) {
+				flag =1;
+				break;
+			}
+		}
+		if(flag == 1) winner = false;
 		return winner;
+
 	}
 }
